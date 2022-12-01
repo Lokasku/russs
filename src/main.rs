9aa7cc97dde72bbd507b7db7ec36b17bd6abd222
Stallwan <stallwan@proton.me>
2022-12-01 13:23:48 +0100

    Functional Version

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,13 @@ use crate::sudoku::Grid;
 fn main() {
     let sdk = read_file("model.txt");
     let mut sdk = Grid { content: sdk };
-    let _solved = sdk.solve();
-    let content = sdk.get_as("line");
-
-    for line in content {
-        println!("{:?}", line);
+    
+    if sdk.solve() {
+        let content = sdk.get_as("line");
+        for line in content {
+            println!("{:?}", line);
+        }
+    } else {
+        panic!("Cannot solve Sudoku.");
     }
 }
